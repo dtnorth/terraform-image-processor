a1fa459495e5aa42c96c836c34a8de22e16c3e06
Derek Northcote <derektnorthcote@gmail.com>
2025-02-09 13:39:05 +0000

    Added runner script

---
+++ github-local-runner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Run linux x64 runner locally
+#
+
+# ../actions-runner/config.sh --url https://github.com/dtnorth/terraform-bjss --token $GITHUB_ACCESS_TOKEN
+
+../actions-runner/run.sh
